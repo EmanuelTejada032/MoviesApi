0ca19d2001aef3d31d4aecd7bb5d87a721489fee
EmanuelTejada032 <emanueltejada31295@gmail.com>
2023-04-27 14:44:37 -0400

    Add Movie controller

---
+++ MoviesApi/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movies.Entities.Movies;
+
+namespace MoviesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MoviesDbContext _movieContext;
+
+        public MoviesController(MoviesDbContext movieContext)
+        {
+            _movieContext = movieContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<Movie>>> GetAll()
+        {
+            var movies = await _movieContext.Movies.Include(movies => movies.Genres).ToListAsync();
+            return Ok(movies);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetAll(int id)
+        {
+            var movies = await _movieContext.Movies
+                .Include( movie => movie.Genres)
+                .Include(movie => movie.MovieRooms)
+                .Include(movie => movie.MoviesActors)
+                .FirstOrDefaultAsync(movie => movie.Id == id);
+            return Ok(movies);
+        }
+
+
+    }
+}
